6c6e21be286f31b10b962c66308ac59868374a47
Rusheel <rusheel2911@gmail.com>
2018-05-12 17:13:53 +0200

    implemented ImageWatermarkSupplier Class

--- iMage/iMage.shutterpile/src/main/java/org/iMage/shutterpile/impl/supplier/ImageWatermarkSupplier.java
+++ iMage/iMage.shutterpile/src/main/java/org/iMage/shutterpile/impl/supplier/ImageWatermarkSupplier.java
@@ -1,7 +1,10 @@
 package org.iMage.shutterpile.impl.supplier;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 
+import org.iMage.shutterpile.impl.filters.GrayscaleFilter;
+import org.iMage.shutterpile.impl.filters.ThresholdFilter;
 import org.iMage.shutterpile.port.IWatermarkSupplier;
 
 /**
@@ -12,6 +15,8 @@ import org.iMage.shutterpile.port.IWatermarkSupplier;
  *
  */
 public final class ImageWatermarkSupplier implements IWatermarkSupplier {
+	
+	BufferedImage watermarkInput;
 
   /**
    * Create the {@link IWatermarkSupplier} by base image of watermark.
@@ -20,14 +25,35 @@ public final class ImageWatermarkSupplier implements IWatermarkSupplier {
    *          the base image to create the watermark
    */
   public ImageWatermarkSupplier(BufferedImage watermarkInput) {
-    // TODO: implement me!
-
+	  GrayscaleFilter grayscale = new GrayscaleFilter();
+	  ThresholdFilter threshold = new ThresholdFilter();
+	  this.watermarkInput = threshold.apply(grayscale.apply(watermarkInput));
+	  
+	  if (!this.watermarkInput.getColorModel().hasAlpha()) {
+			for (int x = 0; x < this.watermarkInput.getWidth(); x++) {
+				for (int y = 0; y < this.watermarkInput.getHeight(); y++) {
+					this.watermarkInput.setRGB(x, y, this.watermarkInput.getRGB(x, y) & 0x00FFFFFF);
+				}
+			}
+		}
+	  
   }
 
   @Override
   public BufferedImage getWatermark() {
-    // TODO: implement me!
-    return null;
+    
+	  BufferedImage watermark = this.watermarkInput;
+	  for (int x = 0; x < watermark.getWidth(); x++) {
+			for (int y = 0; y < watermark.getHeight(); y++) {
+				Color pixel = new Color(watermark.getRGB(x, y));
+				int newAlpha = pixel.getAlpha() / 2;
+				pixel = new Color(pixel.getRed(), pixel.getGreen(), pixel.getBlue(), newAlpha);
+				watermark.setRGB(x, y, pixel.getRGB());
+			}
+		}
+	  
+	  return watermark;
+	  
   }
 
 }
