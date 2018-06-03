1e9495491e633efc86929f06739e150bd88a6236
Rusheel <rusheel2911@gmail.com>
2018-06-03 19:40:37 +0200

    created image viewing pane

--- iMage/iMage.iDeal/src/main/java/iMage/iDeal/mainWindow.java
+++ iMage/iMage.iDeal/src/main/java/iMage/iDeal/mainWindow.java
@@ -1,10 +1,17 @@
 package iMage.iDeal;
 
 import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import org.iMage.shutterpile.*;
 
 public class mainWindow extends JFrame {
 	
@@ -17,7 +24,7 @@ public class mainWindow extends JFrame {
 		setResizable(false);
 		setVisible(true);
 		
-		
+		showPictures();
 		
 	}
 	
@@ -25,6 +32,23 @@ public class mainWindow extends JFrame {
 		
 		JPanel picturePanel = new JPanel();
 		picturePanel.setLayout(new GridLayout(0, 3, 5, 0));
+		
+		try {
+			
+			BufferedImage original = ImageIO.read(new File("src/main/resources/Input.png"));
+			BufferedImage watermark = ImageIO.read(new File("src/main/resources/Watermark.png"));
+			BufferedImage output = ImageIO.read(new File("src/main/resources/Output.png"));
+			
+			JLabel originalPic = new JLabel(new ImageIcon(original));
+			JLabel watermarkPic = new JLabel(new ImageIcon(watermark));
+			JLabel outputPic = new JLabel(new ImageIcon(output));
+			picturePanel.add(originalPic);
+			picturePanel.add(watermarkPic);
+			picturePanel.add(outputPic);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
