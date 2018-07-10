34c6c3ec052563c6879b371b38221f0523cecaec
RusheelIyer <rusheel2911@gmail.com>
2018-07-10 09:49:41 +0200

    fixed errors in the test cases and added global variables

--- iMage/iMage.shutterpile-parallel-acceptance/src/test/java/org/iMage/shutterpile/parallel/acceptance/impl/filters/ParallelTest.java
+++ iMage/iMage.shutterpile-parallel-acceptance/src/test/java/org/iMage/shutterpile/parallel/acceptance/impl/filters/ParallelTest.java
@@ -23,10 +23,11 @@ public class ParallelTest {
 
 	ParallelWatermarkFilter pwf;
 	WatermarkFilter wf;
-	File inputImageFile;
-	File watermarkFile;
-	BufferedImage watermark;
-	BufferedImage inputImage;
+	BufferedImage fOutput;
+	BufferedImage fInput;
+	BufferedImage fWatermark;
+	BufferedImage nfImage;
+	BufferedImage nfWatermark;
 	
 	/**
 	 * set up variables before each test case
@@ -35,11 +36,20 @@ public class ParallelTest {
 	@Before
 	public void setUp() throws Exception {
 		
-		inputImageFile = new File("src/test/resources/F10_Eingabe.png");
-		watermarkFile = new File("src/test/resources/F10_WZ.png");
+		File file = new File("src/test/resources/F10_Ausgabe.png");
+		fOutput = ImageIO.read(file);
 		
-		inputImage = ImageIO.read(inputImageFile);
-		watermark = ImageIO.read(watermarkFile);
+		file = new File("src/test/resources/F10_Eingabe.png");
+		fInput = ImageIO.read(file);
+		
+		file = new File("src/test/resources/F10_WZ.png");
+		fWatermark = ImageIO.read(file);
+		
+		file = new File("src/test/resources/NF10_Bild.png");
+		nfImage = ImageIO.read(file);
+		
+		file = new File("src/test/resources/NF10_WZ.png");
+		nfWatermark = ImageIO.read(file);
 		
 	}
 
@@ -47,30 +57,21 @@ public class ParallelTest {
 	 * test functional requirement 10
 	 */
 	@Test
-	public void applyTest() {
+	public void parallelApplyTest() {
 		
-		File outputImageFile = new File("src/test/resources/F10_Ausgabe.png");
+		//check when thread count automatically generated
+		pwf = new ParallelWatermarkFilter(fWatermark, 2);
+		BufferedImage testOutputNoThread = pwf.apply(fInput);
 		
-		try {
-			BufferedImage outputImage = ImageIO.read(outputImageFile);
-			
-			//check when thread count automatically generated
-			pwf = new ParallelWatermarkFilter(watermark, 2);
-			BufferedImage testOutputNoThread = pwf.apply(inputImage);
-			
-			//check when manual thread count given in constructor
-			pwf = new ParallelWatermarkFilter(watermark, 2, 4);
-			BufferedImage testOutputThread = pwf.apply(inputImage);
-			
-			for (int x = 0; x < outputImage.getWidth(); x++) {
-				for (int y = 0; y < outputImage.getHeight(); y++) {
-					assertEquals(outputImage.getRGB(x, y), testOutputNoThread.getRGB(x, y));
-					assertEquals(outputImage.getRGB(x, y), testOutputThread.getRGB(x, y));
-				}
+		//check when manual thread count given in constructor
+		pwf = new ParallelWatermarkFilter(fWatermark, 2, 4);
+		BufferedImage testOutputThread = pwf.apply(fInput);
+		
+		for (int x = 0; x < fOutput.getWidth(); x++) {
+			for (int y = 0; y < fOutput.getHeight(); y++) {
+				assertEquals(fOutput.getRGB(x, y), testOutputNoThread.getRGB(x, y));
+				assertEquals(fOutput.getRGB(x, y), testOutputThread.getRGB(x, y));
 			}
-			
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 		
 	}
@@ -84,17 +85,17 @@ public class ParallelTest {
 	@Test @Ignore
 	public void sequentialCompareTest() {
 		
-		wf = new WatermarkFilter(watermark, 2);
-		BufferedImage sequential = wf.apply(inputImage);
+		wf = new WatermarkFilter(fWatermark, 2);
+		BufferedImage sequential = wf.apply(fInput);
 		
-		pwf = new ParallelWatermarkFilter(watermark, 2, 2);
-		BufferedImage parallelTwoThread = pwf.apply(inputImage);
+		pwf = new ParallelWatermarkFilter(fWatermark, 2, 2);
+		BufferedImage parallelTwoThread = pwf.apply(fInput);
 		
-		pwf = new ParallelWatermarkFilter(watermark, 2, 8);
-		BufferedImage parallelEightThread = pwf.apply(inputImage);
+		pwf = new ParallelWatermarkFilter(fWatermark, 2, 8);
+		BufferedImage parallelEightThread = pwf.apply(fInput);
 		
-		pwf = new ParallelWatermarkFilter(watermark, 2, 4);
-		BufferedImage parallelFourThread = pwf.apply(inputImage);
+		pwf = new ParallelWatermarkFilter(fWatermark, 2, 4);
+		BufferedImage parallelFourThread = pwf.apply(fInput);
 		
 		for (int x = 0; x < sequential.getWidth(); x++) {
 			for (int y = 0; y < sequential.getHeight(); y++) {
@@ -114,22 +115,12 @@ public class ParallelTest {
 	@Test @Ignore
 	public void sequentialTimeTest() {
 		
-		watermarkFile = new File("src/test/resources/NF10_WZ.png");
-		inputImageFile = new File("src/test/resources/NF10_Bild.png");
+		wf = new WatermarkFilter(nfWatermark, 30);
 		
-		try {
-			watermark = ImageIO.read(watermarkFile);
-			wf = new WatermarkFilter(watermark, 30);
-			inputImage = ImageIO.read(inputImageFile);
-			
-			long runtime = System.currentTimeMillis();
-			wf.apply(inputImage);
-			runtime = System.currentTimeMillis() - runtime;
-			assert (runtime <= 1500);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		long runtime = System.currentTimeMillis();
+		wf.apply(nfImage);
+		runtime = System.currentTimeMillis() - runtime;
+		assert (runtime <= 1500);
 	}
 	
 	/**
@@ -138,29 +129,19 @@ public class ParallelTest {
 	@Test
 	public void parallelSpeedUpTest() {
 		
-		watermarkFile = new File("src/test/resources/NF10_WZ.png");
-		inputImageFile = new File("src/test/resources/NF10_Bild.png");
+		wf = new WatermarkFilter(nfWatermark, 30);
 		
-		try {
-			watermark = ImageIO.read(watermarkFile);
-			wf = new WatermarkFilter(watermark, 30);
-			inputImage = ImageIO.read(inputImageFile);
-			
-			long runtimeSequential = System.currentTimeMillis();
-			wf.apply(inputImage);
-			runtimeSequential = System.currentTimeMillis() - runtimeSequential;
-			
-			pwf = new ParallelWatermarkFilter(watermark, 30);
-			
-			long runtimeParallel = System.currentTimeMillis();
-			pwf.apply(inputImage);
-			runtimeParallel = System.currentTimeMillis() - runtimeParallel;
-			
-			assert (runtimeSequential * 0.75 >= runtimeParallel);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		long runtimeSequential = System.currentTimeMillis();
+		wf.apply(nfImage);
+		runtimeSequential = System.currentTimeMillis() - runtimeSequential;
+		
+		pwf = new ParallelWatermarkFilter(nfWatermark, 30);
+		
+		long runtimeParallel = System.currentTimeMillis();
+		pwf.apply(nfImage);
+		runtimeParallel = System.currentTimeMillis() - runtimeParallel;
+		
+		assert (runtimeSequential * 0.75 >= runtimeParallel);
 	}
 	
 	/**
@@ -169,26 +150,16 @@ public class ParallelTest {
 	@Test
 	public void parallelPerformanceTest() {
 		
-		watermarkFile = new File("src/test/resources/NF10_WZ.png");
-		inputImageFile = new File("src/test/resources/NF10_Bild.png");
+		pwf = new ParallelWatermarkFilter(nfWatermark, 30, 2);
 		
-		try {
-			watermark = ImageIO.read(watermarkFile);
-			inputImage = ImageIO.read(inputImageFile);
-			pwf = new ParallelWatermarkFilter(watermark, 30, 2);
-			
-			long runtime = System.currentTimeMillis();
-			
-			for (int i = 1; i == 10; i++) {
-				pwf.apply(inputImage);
-			}
-			runtime = System.currentTimeMillis() - runtime;
-			
-			assert ((runtime / 10) <= 1000);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
+		long runtime = System.currentTimeMillis();
+		
+		for (int i = 1; i == 10; i++) {
+			pwf.apply(nfImage);
 		}
+		runtime = System.currentTimeMillis() - runtime;
+		
+		assert ((runtime / 10) <= 1000);
 	}
 	
 	/**
@@ -197,32 +168,22 @@ public class ParallelTest {
 	@Test
 	public void parallelDifferenceTest() {
 		
-		watermarkFile = new File("src/test/resources/NF10_WZ.png");
-		inputImageFile = new File("src/test/resources/NF10_Bild.png");
+		pwf = new ParallelWatermarkFilter(nfWatermark, 30, 2);
 		
-		try {
-			watermark = ImageIO.read(watermarkFile);
-			inputImage = ImageIO.read(inputImageFile);
-			pwf = new ParallelWatermarkFilter(watermark, 30, 2);
-			
-			long[] runtime = new long[10];
-			
-			for (int i = 1; i == 10; i++) {
-				long start = System.currentTimeMillis();
-				pwf.apply(inputImage);
-				runtime[i - 1] = System.currentTimeMillis() - start;
+		long[] runtime = new long[10];
+		
+		for (int i = 1; i == 10; i++) {
+			long start = System.currentTimeMillis();
+			pwf.apply(nfImage);
+			runtime[i - 1] = System.currentTimeMillis() - start;
+		}
+		
+		for (int i = 0; i < runtime.length; i++) {
+			int compareTo = i + 1; //compare each value in the array with all elements that come after it
+			while (compareTo < runtime.length) {
+				assert (Math.abs(runtime[i] - runtime[compareTo]) <= 100);
+				compareTo++;
 			}
-			
-			for (int i = 0; i < runtime.length; i++) {
-				int compareTo = i + 1;
-				while (compareTo < runtime.length) {
-					assert ((runtime[i] - runtime[compareTo]) <= 100);
-					compareTo++;
-				}
-			}
-			
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 		
 	}
